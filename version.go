f6c476c2a88d0a172cf43ad57482a193f1759068
heartleo <heartleo@outlook.com>
2026-06-15 13:24:16 +0800

    fix: version from build info

--- version.go
+++ version.go
@@ -1,7 +1,18 @@
 package zlib
 
+import "runtime/debug"
+
 var (
 	Version = "dev"
 	Commit  = "none"
 	Date    = "unknown"
 )
+
+func init() {
+	if Version != "dev" {
+		return
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+}
