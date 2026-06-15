4d275c92072eac592ea65378362132cf58a9a98f
heartleo <heartleo@outlook.com>
2026-06-15 15:23:42 +0800

    fix: advertise only gzip in Accept-Encoding (refs #2)

--- client.go
+++ client.go
@@ -188,7 +188,7 @@ func (c *Client) get(rawURL string) (string, error) {
 	req.Header.Set("User-Agent", defaultUserAgent)
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Encoding", "gzip")
 	req.Header.Set("Sec-Fetch-Dest", "document")
 	req.Header.Set("Sec-Fetch-Mode", "navigate")
 	req.Header.Set("Sec-Fetch-Site", "none")
